d1fa300b3fa3260adffe6dc5725a979647c6bc91
Mariano <mariano.b.fernandez@gmail.com>
2022-01-20 15:01:04 -0300

    add redux

--- src/index.js
+++ src/index.js
@@ -3,9 +3,11 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import "bootstrap/dist/css/bootstrap.min.css";
+import { Provider } from "react-redux";
 
 ReactDOM.render(
-      <App />,
+  <Provider>
+    <App />
+  </Provider>,
   document.getElementById("root")
 );
-
