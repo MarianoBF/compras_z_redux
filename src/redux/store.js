d58fec993732ec31b5d05eb19c4594736ad125e3
Mariano <mariano.b.fernandez@gmail.com>
2022-01-31 19:35:26 -0300

    basic persist setup

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,5 +1,7 @@
 import { createStore, applyMiddleware, compose } from "redux";
 import thunk from "redux-thunk";
+import storage from "redux-persist/lib/storage";
+import { persistStore, persistReducer } from "redux-persist";
 
 import { combineReducers } from "redux";
 import cartReducer from "./reducers/cartReducer";
@@ -7,15 +9,21 @@ import productsReducer from "./reducers/productsReducer";
 
 const rootReducer = combineReducers({
   cart: cartReducer,
-  products: productsReducer
+  products: productsReducer,
 });
 
-const store = createStore(
-  rootReducer,
+const persistConfig = {
+  key: "compras_z_persist",
+  storage,
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+export const store = createStore(
+  persistedReducer,
   compose(
     applyMiddleware(thunk),
     window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
   )
 );
-
-export default store;
+export const persistor = persistStore(store);
